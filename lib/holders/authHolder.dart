9b5654d54fc65f4682c15b72bf3c9efd3b105635
Harmanjit Singh <harmanjit140500@gmail.com>
2021-05-03 01:48:38 +0530

    log_holder

--- lib/holders/authHolder.dart
+++ lib/holders/authHolder.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:huddle/models/color.dart';
 
 class AuthHolder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: colorFile.base,
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Container(
+            height: 200,
+            // color: Colors.green,
+            child: Image.asset("assets/waiting.gif"),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/models/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorFile {
+  final Color base = Color(0xff2b2e4a);
+  final Color primary = Color(0xffe84545);
+  final Color secondary = Color(0xff903749);
+  final Color baseSecondary = Color(0xff53354a);
+}
+
+final ColorFile colorFile = new ColorFile();
